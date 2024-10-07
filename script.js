692e63e0525a91c11e91181bd34e444e58a0a2ec
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-10-07 20:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+document.getElementsByTagName('strong');
+
 function highlight() {
     //Write your code here
-
-
+	var strongElement = document.getElementsByTagName('strong');
+	for(let i = 0; i < strongElement.length; i ++){
+		strongElement[i].style.color = 'green';
+	}
+	
 }
 
 
 function return_normal() {
     //Write your code here
+	var strongElement = document.getElementsByTagName('strong');
+
+	for(var i = 0; i < strongElement.length; i ++){
+		strongElement[i].style.color = 'black';
+	}
 
     
 }
